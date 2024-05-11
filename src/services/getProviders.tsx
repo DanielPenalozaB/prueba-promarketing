e614b05524da2a35d5a040e055c0c4a98371684b
Daniel Penaloza <juandanielpenalozabrito@gmail.com>
2024-05-11 11:03:11 -0500

    feat: :sparkles: Add the getProviders() service function
    Add the getProviders() service to structure it's logic and functionality

---
+++ src/services/getProviders.tsx
@@ -0,0 +1,23 @@
+import { ProviderInterface } from "@/interfaces";
+import { url } from "inspector";
+import React from "react";
+
+export default async function getProviders(): Promise<
+	ProviderInterface[] | string
+> {
+	const PAYLOAD = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		url: `${process.env.NEXT_PUBLIC_API_BASE_URL}/provider`,
+	};
+
+	try {
+		const response = await fetch(PAYLOAD.url, PAYLOAD);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		return "Error fetching providers";
+	}
+}
